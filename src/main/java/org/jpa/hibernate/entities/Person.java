bd73c4cb173e3fce70b232156953b25ff6759797
Ali Bostanpira <alibostanpira@gmail.com>
2024-01-08 02:33:48 +0330

    Person Entity configuration completed.

--- src/main/java/org/jpa/hibernate/entities/Person.java
+++ src/main/java/org/jpa/hibernate/entities/Person.java
@@ -1,2 +1,19 @@
-package org.jpa.hibernate.entities;public class Person {
+package org.jpa.hibernate.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
 }
