8e6ec0438b602a655b3553b5cac70f97c4de3a5e
Ali Bostanpira <alibostanpira@gmail.com>
2024-01-08 02:25:25 +0330

    Database configuration is done

--- src/main/java/org/jpa/hibernate/JpaHibernateApplication.java
+++ src/main/java/org/jpa/hibernate/JpaHibernateApplication.java
@@ -2,8 +2,10 @@ package org.jpa.hibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class JpaHibernateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpaHibernateApplication.class, args);
---
+++ src/main/java/org/jpa/hibernate/config/AuditAwareImpl.java
@@ -0,0 +1,17 @@
+package org.jpa.hibernate.config;
+
+import io.micrometer.common.lang.NonNullApi;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@NonNullApi
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("Ali");
+    }
+}
