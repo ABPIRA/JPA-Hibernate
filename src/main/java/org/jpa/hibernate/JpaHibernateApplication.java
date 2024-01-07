72e3561bec78b456633a74f01ac082192c21d4b2
ABPIRA <ali.bostanpira.abpira@gmail.com>
2024-01-07 20:34:30 +0100

    Project Initialized

---
+++ src/main/java/org/jpa/hibernate/JpaHibernateApplication.java
@@ -0,0 +1,11 @@
+package org.jpa.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateApplication.class, args);
+	}
+}
---
+++ src/test/java/org/jpa/hibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jpa.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
